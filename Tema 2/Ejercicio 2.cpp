cb4b529747db1b80f3da9e0a29799fea7ee7c616
NickolasVB <143093793+NickolasVB@users.noreply.github.com>
2023-09-20 20:32:58 -0500

    Delete Tema 2 directory

--- Tema 2/Ejercicio 2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(){
-
-    cout << "   CCC   +      +  \n  C      +      +\n C     +++++  +++++\n  C      +      +\n   CCC   +      +" <<endl;
-}
